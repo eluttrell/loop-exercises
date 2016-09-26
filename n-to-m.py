94bfa1d495c56128efa4b29a63679fccb936c56f
Elijah Allstrom-Luttrell <elieel@Elijah-Allstrom-Luttrells-iMac.local>
2016-09-25 23:24:42 -0400

    Start work on loop exercises. Complete first few exercises, last three + bonuses remain.

---
+++ n-to-m.py
@@ -0,0 +1,4 @@
+Sn = int(raw_input("Start nmber: "))
+En = int(raw_input("End number: "))
+for numbers in range (Sn, En + 1):
+    print numbers
---
+++ print-a-square-ii.py
@@ -0,0 +1,3 @@
+size = int(raw_input("How big do you want the square? "))
+for square in range(size):
+    print "*" * size
